881718365dceef9d9625081b6dacb198b4fd8041
holly <holler4amarshall@hotmail.com>
2016-11-21 13:15:51 +0000

    add test & feature to add and return data from api/posts

--- posts/api.py
+++ posts/api.py
@@ -12,6 +12,11 @@ from .database import session
 @app.route("/api/posts", methods=["GET"])
 def posts_get():
     """ Get a list of posts """
-    data = json.dumps([])
+
+    # Get the posts from the database
+    posts = session.query(models.Post).order_by(models.Post.id)
+
+    # Convert the posts to JSON and return a response
+    data = json.dumps([post.as_dictionary() for post in posts])
     return Response(data, 200, mimetype="application/json")
 
