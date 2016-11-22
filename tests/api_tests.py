d5d475eb1b16fba978734600593d9c173b7020f4
holly <holler4amarshall@hotmail.com>
2016-11-22 04:16:59 +0000

    add a test for query param to filter posts by title

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -34,8 +34,8 @@ class TestAPI(unittest.TestCase):
     
         data = json.loads(response.data.decode("ascii"))
         self.assertEqual(data, [])
-        
-    '''
+    
+    '''    
     def test_unsupported_accept_header(self):
         response = self.client.get("/api/posts",
             headers=[("Accept", "application/xml")]
@@ -47,8 +47,7 @@ class TestAPI(unittest.TestCase):
         data = json.loads(response.data.decode("ascii"))
         self.assertEqual(data["message"],
                          "Request must accept application/json data")
-    '''
-        
+    '''    
         
     def test_get_posts(self):
         """ Getting posts from a populated database """
@@ -120,6 +119,35 @@ class TestAPI(unittest.TestCase):
 
         posts = session.query(models.Post).all()
         self.assertEqual(len(posts), 0) 
+        
+        
+    def test_get_posts_with_title(self):
+        """ Filtering posts by title """
+        postA = models.Post(title="Post with bells", body="Just a test")
+        postB = models.Post(title="Post with whistles", body="Still a test")
+        postC = models.Post(title="Post with bells and whistles",
+                            body="Another test")
+
+        session.add_all([postA, postB, postC])
+        session.commit()
+
+        response = self.client.get("/api/posts?title_like=whistles",
+            headers=[("Accept", "application/json")]
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "application/json")
+
+        posts = json.loads(response.data.decode("ascii"))
+        self.assertEqual(len(posts), 2)
+
+        post = posts[0]
+        self.assertEqual(post["title"], "Post with whistles")
+        self.assertEqual(post["body"], "Still a test")
+
+        post = posts[1]
+        self.assertEqual(post["title"], "Post with bells and whistles")
+        self.assertEqual(post["body"], "Another test")
 
         
 
